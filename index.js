19c06f5917444658682f9e60efe5d93e2ee38140
suryaram54 <rms630136@gmail.com>
2022-03-29 15:34:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// const http=require('http')
+// http.createServer((req,resp)=>{
+//       resp.write("anjani anusha");
+//       resp.end();
+
+
+// }).listen(4500);
+var colors = require('colors');
+// var colors = require('chalk');
+// import chalk from 'chalk';
+
+// console.log(chalk.blue('Hello world!'));
+ 
+console.log('hello'.green);
+// import chalk from 'chalk';
+
+// console.log(chalk.blue('Hello world!'))
